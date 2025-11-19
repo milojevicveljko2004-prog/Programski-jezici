ebf81040b7312e8b2643b100c597ab3297b2feaf
Veljko Milojevic <milojevicveljko2004@gmail.com>
2025-11-19 19:59:54 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Korpa k = new Korpa();
+
+                k.ucitaj("ulaz.txt");
+                //print je samo za DEBUG
+                k.print();
+
+                k.remove(StatusProizvoda.NarucenOdProizvodjaca);
+                k.upisi("izlaz.txt");
+                Console.WriteLine("Nakon izbacivanja: ");
+                k.print();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Greska: " + e);
+            }
+        }
+    }
+}
